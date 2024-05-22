65e306a7be737b57bd9281a7243952c259df5d09
Ghost695 <80474288+Ghost695@users.noreply.github.com>
2024-05-22 19:10:29 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-inst_login = ''
-inst_pwd = ''
-mega_login = ''
-mega_pwd = ''
-lang = 'ru'
+inst_login = '' #логин
+inst_pwd = '' #пароль
+mega_login = '' #логин
+mega_pwd = '' #пароль
+lang = 'ru' #выбор языка если укр то ua
